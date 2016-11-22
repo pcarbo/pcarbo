b349c54e266b71ea1dce1975882852795eadbb7d
Peter Carbonetto <peter.carbonetto@gmail.com>
2016-11-22 13:23:56 -0600

    Added description, usage & comments to gen_many_normal_datasets.sh.

--- sbatch/gen_many_normal_datasets.sh
+++ sbatch/gen_many_normal_datasets.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-# Explain here what this command does, and how to use it.
+# This bash script illustrates how one can implement a bash script
+# combined with SLURM to schedule several jobs simultaneously. For
+# example, to generate 5 data sets in R using script gen.rnorm.data.R,
+# enter the following in the shell command line:
 #
 #   bash ./gen_many_normal_datasets.sh 5
 #
+# In this example, we generate data sets with 1000 samples (rows) and
+# 20 features (columns), but this is easily modified in the for-loop
+# below.
+
+# Get the script parameter specified as a command-line argument.
 NUMDATASETS=${1}
 
+# Schedule a separate job in SLURM for each data set that we would
+# like to generate.
 for (( i=1 ; i <= ${NUMDATASETS} ; i++ )); do
   sbatch --job-name=gen-rnormdata-sbatch-${i} \
          --output=../results/sbatch-${i}.out \
          --error=../results/sbatch-${i}.err \
-         gen_rnorm_data.sbatch ../results/normal-data-${i}.txt ${i} 20 4
+         gen_rnorm_data.sbatch ../results/normal-data-${i}.txt ${i} 1000 20
 done

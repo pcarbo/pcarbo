a9cd672b98e71e1cea3e88798c0fe2c7a971e153
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-01-17 21:36:20 -0600

    Fixed up demo.noisy.logistic.R.

--- R/demo.noisy.logistic.R
+++ R/demo.noisy.logistic.R
@@ -1,4 +1,5 @@
-# TO DO: Explain here what this script does, and how to use it.
+# Demonstration estimation of noise parameter in "noisy" regression
+# model is not possible.
 library(ggplot2)
 library(ggthemes)
 
@@ -7,20 +8,21 @@ library(ggthemes)
 n    <- 500      # Number of data samples.
 ns   <- 1000     # Number of Monte Carlo samples.
 se   <- sqrt(2)  # True standard deviation of "noise".
-beta <- (-1.5)   # True coefficient.
+beta <- (-2)     # True coefficient.
 
 # Grid of candidate values for the logistic regression coefficient
 # (beta) and the standard deviation of the noise (se).
 b <- seq(-5,5,0.2)
-s <- 10^seq(-3,3,0.2)
+s <- 10^seq(-3,3,0.1)
 
-# USEFUL FUNCTIONS
-# ----------------
+# SOME USEFUL FUNCTIONS
+# ---------------------
 # Sigmoid of x.
 sigmoid <- function (x)
   1/(1 + exp(-x))
 
-# Compute log-likelihood for logistic regression model.
+# Compute log-likelihood for logistic regression model (as it is
+# defined below).
 computeloglikelihood <- function (counts, b, e)
   counts["-1","0"] * log(1 - sigmoid(e - b)) +
   counts[ "1","0"] * log(1 - sigmoid(e + b)) +
@@ -37,19 +39,17 @@ mclogint <- function (x)
 
 # GENERATE DATA SET
 # -----------------
+# Generate binary observations (fair coin with values of -1 or +1).
 cat("Generating data set.\n")
-# Generate a binary observation (fair coin -1 or +1).
+set.seed(1)
 x <- 1 - 2*as.numeric(runif(n) < 0.5)
-
-# Generate "noise".
-e <- se * rnorm(n)
 
 # Simulate coin flips y such that
 #
 #   Pr(y = 1) = f(x*beta + e), 
 #
-# where f is the sigmoid function.
-y <- as.numeric(runif(n) < sigmoid(x*beta + e))
+# where f is the sigmoid function, and e ~ N(0,se^2)
+y <- as.numeric(runif(n) < sigmoid(x*beta + se * rnorm(n)))
 
 # Check the data.
 print(summary(factor(x)))
@@ -59,21 +59,24 @@ print(mean(x[y == 1]))
 
 # COMPUTE LOG-LIKELIHOOD SURFACE
 # ------------------------------
-cat("Computing log-likelihood surface.\n")
+cat("Computing log-likelihood for each candidate combination.\n")
 counts <- table(x = factor(x),y = factor(y))
-params <- cbind(expand.grid(b = b,s = s),
-                data.frame(L = 0))
-for (i in 1:nrow(params)) {
+out    <- cbind(expand.grid(beta = b,se = s),
+                data.frame(logw = 0))
+for (i in 1:nrow(out)) {
   
-  # Compute a Monte Carlo estimate of the log-likelihood.
-  e             <- params[i,"s"] * rnorm(ns)
-  params[i,"L"] <- mclogint(computeloglikelihood(counts,params[i,"b"],e))
+  # Compute a simple Monte Carlo estimate of the log-likelihood.
+  b <- out$beta[i]
+  s <- out$se[i]
+  out$logw[i] <- mclogint(computeloglikelihood(counts,b,s*rnorm(ns)))
 }
 
 # Plot the likelihood surface (up to a normalizing constant).
-print(ggplot(params,aes(x = b,y = log10(s),colour = exp(L - max(L)))) +
+out <- transform(out,w = exp(logw - max(logw)))
+print(ggplot(out,aes(x = beta,y = log10(se),colour = w)) +
       geom_point() +
-      scale_x_continuous(breaks = seq(min(b),max(b))) +
+      scale_x_continuous(breaks = seq(min(out$beta),max(out$beta))) +
+      scale_y_continuous(breaks = seq(min(log10(out$se)),max(log10(out$se)))) +
       scale_color_gradient(low = "lightcyan",high = "orangered") +
       theme_minimal() +
       theme(panel.grid.major = element_blank(),

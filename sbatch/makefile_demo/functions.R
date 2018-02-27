2511b17b7c9b8877111037db788b16107c0e2b97
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-02-27 08:37:17 -0600

    Combined all the R functions for the sbatch Makefile demo to one file, functions.R.

--- sbatch/makefile_demo/functions.R
+++ sbatch/makefile_demo/functions.R
@@ -1,3 +1,59 @@
+# Shorthand for machine precision.
+eps <- .Machine$double.eps
+
+# Subtract b[i] from each column A[,i].
+subtract.cols <- function (A, b)
+  t(t(A) - b)
+
+# Scale each column A[,i] by b[i].
+scale.cols <- function (A, b)
+  t(t(A) * b)
+
+# Simulate n random numbers X generated as follows: (1) with
+# probability w[i], sample U from the univariate normal with zero mean
+# and standard deviation s[i]; (2) sample X from the univariate normal
+# with mean U and standard deviation se. This is the "Extreme
+# Deconvolution" model for the special case when d = 1.
+# 
+# Input arguments w and and se must be vectors of the same length, and
+# se specifies the standard devation in the "noise" for each of the
+# samples, so it should be a vector of length n.
+datasim.norm <- function (w, s, se) {
+
+  # Get the number of mixture components (k) and the number of samples
+  # to simulate (n).
+  k <- length(w)
+  n <- length(se)
+    
+  # Draw the source mixture component for each sample.
+  i <- sample(k,n,replace = TRUE,prob = w)
+  
+  # Draw U for each sample.
+  u <- rnorm(n = n,sd = s[i])
+
+  # Draw X for each sample.
+  return(rnorm(n = n,mean = u,sd = se))
+}
+
+# Compute the n x k conditional likelihood matrix, where n is the
+# number of samples and k is the number of mixture components, for the
+# case when the likelihood is univariate normal and prior is a mixture
+# of univariate normals.
+condlikmatrix.norm <- function (x, se, s) {
+
+  # Get the number of samples (n) and the number of mixture components (k).
+  n <- length(x)
+  k <- length(s)
+
+  # Entry (i,j) of the conditional likelihood matrix is equal to
+  # N(0,se[i]^2 + s[j]^2), the normal density with zero mean and
+  # variance se[i]^2 + s[j]^2.
+  L <- matrix(0,n,k)
+  for (j in 1:k)
+    L[,j] <- dnorm(x,sd = sqrt(se^2 + s[j]^2))
+  return(L)
+}
+
 # Compute the mixture distribution objective function given n x k
 # conditional likelihood matrix L and mixture weights w, where n is
 # the number of samples and k is the number of mixture components.

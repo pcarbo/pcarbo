996d8bab7b62c11816489ad3e447b89dcf95704a
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-04-26 17:46:58 -0500

    Improved the second plot.

--- R/varbvs.plot.R
+++ R/varbvs.plot.R
@@ -146,7 +146,7 @@ klmin <- min(dat[,"KL"])
 dat   <- as.data.frame(dat)
 dat   <- transform(dat,
                    beta1 = alpha1*mu1,
-                   dist  = log10(KL - klmin + 1e-8))
+                   dist  = KL - klmin + 1e-8)
 p2 <- ggplot(dat,aes(x = beta1,y = dist)) +
     geom_line()
 

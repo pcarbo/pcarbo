3a282f680b532d11bbd82e6755b93824ebbb9b29
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-05-10 15:21:12 -0500

    Implemented algorithm to fit negative binomial model.

--- R/negbinfit.R
+++ R/negbinfit.R
@@ -1,10 +1,13 @@
-# TO DO: Explain here what this script is for.
+# A short script illustrating how to implement a simple co-ordinate
+# ascent algorithm to fit a negative binomial model when the samples
+# are weighted.
 
 # SIMULATE DATA
 # -------------
 # Draw samples from the negative binomial distribution with mean
 # t*(1-p)/p and variance t*(1-p)/p^2. Randomly assign weights to the
-# samples.
+# samples. Variables t and p specify the "size" and "prob" arguments,
+# respectively, to the "dnbinom" function.
 set.seed(1)
 n <- 200
 t <- 2
@@ -12,33 +15,32 @@ p <- 0.1
 x <- rnbinom(n,t,p)
 w <- rep(1,n)
 
-# Plot the log-likelihood surface.
-params <- expand.grid(t = seq(0.1,5,0.25),p = seq(0.01,0.25,0.01))
-params <- cbind(params,data.frame(logl = 0))
-m      <- nrow(params)
-for (i in 1:m) {
-  t <- params[i,"t"]
-  p <- params[i,"p"]
-  params[i,"logl"] <- sum(w * dnbinom(x,t,p,log = TRUE))
-}
-library(ggplot2)
-library(cowplot)
-ggplot(params,aes(x = t,y = p,z = logl)) +
-       geom_contour(color = "dodgerblue",bins = 50)
+# Without loss of generality, normalize the probabilities so that sum
+# to 1.
+w <- w/sum(w)
 
 # FIT MODEL
 # ---------
-# Repeat for 100 iterations.
+# Initialize the estimates of t and p.
 t <- 4
 p <- 0.5
-for (i in 1:100) {
+
+# Repeat the co-ordinate ascent updates for 50 iterations.
+tmin <- 0.01
+tmax <- 100
+cat("iter log-likelihood\n")
+for (i in 1:50) {
 
   # Update the success probability (p) with the "size" parameter (t)
   # fixed.
-  y <- t*sum(w)/sum(w*x)
+  y <- t/sum(w*x)
   p <- y/(y + 1)
 
   # Update the "size" parameter (t) with the success probability (p)
   # fixed.
-  # TO DO.
+  g <- function (t) sum(w * digamma(x + t)) - digamma(t) + log(p)
+  t <- uniroot(g,c(tmin,tmax))$root
+
+  # Print the log-likelihood at the current solution.
+  cat(sprintf("%4d %+0.11f\n",i,sum(w * dnbinom(x,t,p,log = TRUE))))
 }

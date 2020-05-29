a3b911de3485c31d4f169bb5619fa71e2077bc6f
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-05-29 07:29:06 -0500

    Modified negbin EM algorithm to allow for weighted samples.

--- R/negbin.R
+++ R/negbin.R
@@ -14,6 +14,12 @@ a <- 20   # number of successes ("size").
 p <- 0.1  # success rate.
 x <- rnbinom(n,a,p)
 
+# Randomly assign weights to the samples. When all the weights are the
+# same, the EM algorithm should recover the maximum-likelihood
+# estimate (MLE) computed by fitdistr.
+# w <- runif(n)
+w <- rep(1,n)
+
 # Estimate the "size" (a) and success rate (p) parameters of the
 # negative binomial using fitdistr.
 fit <- fitdistr(x,"negative binomial")
@@ -22,15 +28,15 @@ mu  <- fit$estimate["mu"]
 fit <- list(a = a,p = a/(a + mu))
 
 # Compute the log-likelihood for the negative binomial.
-nbinom_loglik <- function (x, a, p) {
+nbinom_loglik <- function (x, w, a, p, e = 1e-14) {
   a <- pmax(a,e)
   p <- pmin(pmax(p,e),1-e)
-  return(sum(dnbinom(x,a,p,log = TRUE)))
+  return(sum(w*dnbinom(x,a,p,log = TRUE)))
 }
 
 # This implements a single EM update using the expressions given in
 # Adamidis (1999) and Huang et al (2019).
-nbinom_update <- function (x, a, p, e = 1e-14) {
+nbinom_update <- function (x, w, a, p, e = 1e-14) {
 
   # Make sure a > 0 and p is [0,1].
   a <- pmax(a,e)
@@ -43,8 +49,8 @@ nbinom_update <- function (x, a, p, e = 1e-14) {
     
   # M-step
   # ------
-  p <- sum(b*d)/sum(x + (b-1)*d)
-  a <- -mean(d)/log(p)
+  p <- sum(b*w*d)/sum(w*(x + (b-1)*d))
+  a <- -sum(w*d)/(log(p)*sum(w))
   
   # Make sure a > 0 and p is [0,1].
   a <- pmax(a,e)
@@ -52,23 +58,23 @@ nbinom_update <- function (x, a, p, e = 1e-14) {
   return(list(a = a,p = p))
 }
 
-# Fit negative binomial model using EM algorithm, as described in
-# Adamidis (1999) and Huang et al (2019).
+# Fit negative binomial model using EM, as it is described in Adamidis
+# (1999) and Huang et al (2019).
 p       <- 0.5  # Initial estimate of success rate.
 a       <- 1    # Initial estimate of "size".
 numiter <- 500
 loglik  <- rep(0,numiter)
 for (i in 1:numiter) {
-  out       <- nbinom_update(x,a,p)
+  out       <- nbinom_update(x,w,a,p)
   a         <- out$a
   p         <- out$p
-  loglik[i] <- nbinom_loglik(x,a,p)
+  loglik[i] <- nbinom_loglik(x,w,a,p)
 }
 
 # See if daarem finds a good solution more quickly than basic EM.
 daarfit <- daarem(c(1,0.5),
-                  function (par) unlist(nbinom_update(x,par[1],par[2])),
-                  function (par) nbinom_loglik(x,par[1],par[2]))
+                  function (par) unlist(nbinom_update(x,w,par[1],par[2])),
+                  function (par) nbinom_loglik(x,w,par[1],par[2]))
 
 # The solution should improve at each iteration.
 loglik.best <- daarfit$value.objfn
@@ -82,9 +88,9 @@ lines(1:length(daarfit$objfn.track),loglik.best - daarfit$objfn.track + 1e-4,
       col = "darkorange",lwd = 2)
 
 # Compare the EM and "fitdistr" solutions.
-cat(sprintf("fitdistr loglik: %0.12f\n",nbinom_loglik(x,fit$a,fit$p)))
-cat(sprintf("EM loglik:       %0.12f\n",nbinom_loglik(x,a,p)))
-cat(sprintf("daarem loglik:   %0.12f\n",nbinom_loglik(x,daarfit$par["a"],
+cat(sprintf("fitdistr loglik: %0.12f\n",nbinom_loglik(x,w,fit$a,fit$p)))
+cat(sprintf("EM loglik:       %0.12f\n",nbinom_loglik(x,w,a,p)))
+cat(sprintf("daarem loglik:   %0.12f\n",nbinom_loglik(x,w,daarfit$par["a"],
                                                       daarfit$par["p"])))
 cat(sprintf("             a      p\n"))
 cat(sprintf("fitdistr %5.2f %0.4f\n",fit$a,fit$p))

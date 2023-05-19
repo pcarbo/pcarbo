2ce29e8b96d2dd7bc32ae2798b020e142b823208
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-05-19 08:35:25 -0500

    Made a few small improvements to the importance sampling demo (is_demo.R).

--- R/is_demo.R
+++ R/is_demo.R
@@ -1,4 +1,6 @@
-# TO DO: Explain here what this script is for, and how to use it.
+# In this script we illustrate computing moments and other integrals
+# for a polynomial exponential distribution in a numerically stable
+# way using importance sampling.
 library(pracma)
 set.seed(1)
 
@@ -6,8 +8,11 @@ set.seed(1)
 a <- -6
 b <- 6
     
-# Plot the polynomial density.
+# These are the coefficients of the polynomial (first input passed
+# to "polyval").
 p <- c(-0.05,0.1,0.3,-0.2,0.1)
+
+# Plot the polynomial exponential density.
 logf <- function (x)
   polyval(p,x)
 f <- function (x)
@@ -17,54 +22,59 @@ x <- seq(a,b,length.out = n0)
 y <- f(x)
 plot(x,y/sum(y),type = "l",lwd = 3,col = "cyan",ylab = "pdf(x)")
 
-# Compute the mean and standard deviation under the polynomial density
-# using quadrature.
+# First, compute the mean and standard deviation under the polynomial
+# exponentiial density using quadrature with a low tolerance (for
+# better accuracy). We'll use this as a point of comparison for our
+# importance sampling estimates.
 Z  <- quad(function (x) f(x),a,b,tol = 1e-14)
 m  <- quad(function (x) x*f(x)/Z,a,b,tol = 1e-14)
 m2 <- quad(function (x) x^2*f(x)/Z,a,b,tol = 1e-14)
 s  <- sqrt(m2 - m^2)
 cat("Quadrature estimates:\n")
-cat(sprintf("mean: %0.8f\n",m))
-cat(sprintf("s.d.: %0.8f\n",s))
-cat(sprintf("logZ: %0.8f\n",log(Z)))
+cat(sprintf("mean=%0.8f\n",m))
+cat(sprintf("s.d.=%0.8f\n",s))
+cat(sprintf("logZ=%0.8f\n",log(Z)))
 
-# Draw samples uniformly "at random" from the interval [a,b].
+# Draw a small number of "random samples" uniformly from the interval
+# [a,b].
 n <- 24
 x <- seq(a,b,length.out = n)
-# x <- sort(runif(n,a,b))
 
 # Compute the log-importance weights.
 logw <- logf(x)
 
 # This function takes as input an array of unnormalized
 # log-probabilities logw and returns normalized probabilities such
-a# that the sum is equal to 1.
+# that the sum is equal to 1.
 normalizelogweights <- function (logw) {
-
-  # Guard against underflow or overflow by adjusting the
-  # log-probabilities so that the largest probability is 1.
   c <- max(logw)
   w <- exp(logw - c)
-
-  # Normalize the probabilities.
   return(w/sum(w))
 }
 
-# TO DO: Explain here what this function does, and how to use it.
+# This function computes log(mean(w)) given input logw = log(w) in a
+# numerically stable way.
 logmean <- function (logw) {
   c <- max(logw)
   w <- exp(logw - c)
   return(c + log(mean(w)))
 }
 
-# Compute the normalized importance weights.
+# Estimate the logarithm of the normalizing constant, Z, such that
+# f(x)/Z integrates to 1, and where f(x) is the polynomial exponential
+# density.
 logZ <- logmean(logw) + log(b - a)
-w  <- normalizelogweights(logw)
+
+# Compute the normalized importance weights, and plot them.
+w <- normalizelogweights(logw)
+lines(x,w*n/n0,lwd = 1,col = "darkblue")
+points(x,w*n/n0,pch = 20,cex = 0.65,col = "darkblue")
+
+# Compute importance sampling estimates of the mean and s.d. under the
+# polynomial exponential density.
 m  <- sum(x*w)
 m2 <- sum(x^2*w)
 s  <- sqrt(m2 - m^2)
-lines(x,w*n/n0,lwd = 1,col = "darkblue")
-points(x,w*n/n0,pch = 20,cex = 0.65,col = "darkblue")
 cat("Monte Carlo estimates:\n")
 cat(sprintf("mean: %0.8f\n",m))
 cat(sprintf("s.d.: %0.8f\n",s))
